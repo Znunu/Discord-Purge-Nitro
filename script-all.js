bd1a298e5908dd901d6cbb06c01e9b96e85ed1dd
Znunu <61394004+Znunu@users.noreply.github.com>
2021-07-16 20:46:15 +0200

    Update and rename script-all.js to style.css

--- script-all.js
+++
@@ -1,5 +0,0 @@
-const buttonsToHide = ["Open GIF picker", "Open sticker picker", "Send a gift"];
-let css = "";
-buttonsToHide.forEach(button => css = css.concat(`[aria-label="${button}"]{display:none}`));
-const style = document.createElement('style'); style.innerHTML = css;
-document.head.appendChild(style);
